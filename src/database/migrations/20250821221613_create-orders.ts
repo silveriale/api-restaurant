f5bc3d842fac2b494f0deea63176d2e8d2457158
silveriale <leticia.silveria.costa@gmail.com>
2025-08-21 19:28:00 -0300

    Migration de pedidos
    - npm run knex -- migrate:make create-orders
    - npm run knex -- migrate:latest
    - nova tabela chamada "orders" para gerenciar os pedidos
    - estrutura da tabela, incluindo a relação com outras tabelas, como a importância de manter o preço separado na tabela de pedidos para preservar o histórico de preços
    - colunas como chave primária, referências a outras tabelas, tipos de dados e a importância de manter o preço na tabela de pedidos
    - migration ao final para criar a tabela no banco de dados.

---
+++ src/database/migrations/20250821221613_create-orders.ts
@@ -0,0 +1,23 @@
+import type { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable("orders", (table) => {
+    table.increments("id").primary(), // Cria a coluna id como chave primária e auto-incremento
+      table
+        .integer("table_session_id")
+        .notNullable()
+        .references("id")
+        .inTable("tables_sessions"), // Cria a coluna table_session_id que não pode ser nula e referencia a tabela tables_sessions
+      table
+        .integer("product_id")
+        .notNullable()
+        .references("id")
+        .inTable("products"), // Cria a coluna product_id que não pode ser nula e referencia a tabela products
+      table.integer("quantity").notNullable(), // Cria a coluna quantity que não pode ser nula
+      table.decimal("price").notNullable(), // Cria a coluna price que não pode ser nula
+      table.timestamp("created_at").defaultTo(knex.fn.now()), // Cria a coluna created_at com o horário atual como padrão
+      table.timestamp("update_at").nullable(); // Cria a coluna update_at que pode ser nula
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {}
