3b0f0cf48c9174f91d984f922d751915a6f1abef
silveriale <leticia.silveria.costa@gmail.com>
2025-08-21 18:12:45 -0300

    Rota e Controller de sessão da mesa
    -  como criar um controller e uma rota para manipular uma tabela no banco de dados.

--- src/database/migrations/20250821194827_create-tables-sessions.ts
+++ src/database/migrations/20250821194827_create-tables-sessions.ts
@@ -2,10 +2,14 @@ import type { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable("tables_sessions", (table) => {
-    table.increments("id").primary(); // cria a coluna id como chave primária
-    table.integer("table_id").notNullable().references("id").inTable("tables"); // cria a coluna table_id como chave estrangeira referenciando a tabela "tables"
-    table.timestamp("opened_at").defaultTo(knex.fn.now()); // cria a coluna opened_at com o timestamp atual por padrão, p saber quando a mesa foi aberta
-    table.timestamp("closed_at"); // cria a coluna closed_at que pode ser nula, p saber se a mesa foi fechada ou não
+    table.increments("id").primary(), // cria a coluna id como chave primária
+      table
+        .integer("table_id")
+        .notNullable()
+        .references("id")
+        .inTable("tables"), // cria a coluna table_id como chave estrangeira referenciando a tabela "tables"
+      table.timestamp("opened_at").defaultTo(knex.fn.now()), // cria a coluna opened_at com o timestamp atual por padrão, p saber quando a mesa foi aberta
+      table.timestamp("closed_at"); // cria a coluna closed_at que pode ser nula, p saber se a mesa foi fechada ou não
   });
 }
 
