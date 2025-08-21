99a6468e76039965fdb98cdd4de910e996239ec3
silveriale <leticia.silveria.costa@gmail.com>
2025-08-21 13:55:11 -0300

    Adicionando e configurando Zod
    -  npm i zod@3.23.8
    -  instalação do Zod para validação de dados nas requisições
    - omo tratar exceções geradas por validações com o Zod, importando o Zod e a classe ZodError
    - foi feito uma verificação de instância de erro com ZodError e, se for o caso, retornar um status 400 com mensagens de erro formatadas

--- src/middlewares/error-handling.ts
+++ src/middlewares/error-handling.ts
@@ -1,15 +1,30 @@
 import { Request, Response, NextFunction } from "express";
 import { AppError } from "@/utils/AppError";
+import { ZodError } from "zod";
 
-export function errorHandling(error: any, request: Request, response: Response, _: NextFunction) { // middleware para tratamento de erros
-  if (error instanceof AppError) { // verifica se o erro é uma instância de AppError
-    return response.status(error.statusCode).json({ // retorna o erro com o código de status e mensagem
+export function errorHandling(
+  error: any,
+  request: Request,
+  response: Response,
+  _: NextFunction
+) {
+  // middleware para tratamento de erros
+  if (error instanceof AppError) {
+    // verifica se o erro é uma instância de AppError
+    return response.status(error.statusCode).json({
+      // retorna o erro com o código de status e mensagem
       message: error.message,
     });
   }
 
-  return response.status(500).json({ // retorna erro genérico para outros tipos de erro
-    message: error.message,
-  });
-    
+  if (error instanceof ZodError) {
+    // verifica se o erro é uma instância de ZodError
+    return response.status(400).json({
+      // retorna erro de validação
+      message: "Validation error", // mensagem de erro
+      issues: error.format(), // formata os erros de validação
+    });
+  }
+
+  return response.status(500).json({ message: error.message }); // retorna erro genérico para outros tipos de erro
 }
