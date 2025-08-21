40419d92937eaf0149b0d7a6d47d8dd65f418941
silveriale <leticia.silveria.costa@gmail.com>
2025-08-21 15:01:23 -0300

    Listando produtos
    - como organizar uma requisição para listar produtos cadastrados em um banco de dados
    - método de index para listar os produtos, e é mostrado como ordenar os resultados alfabeticamente
    - como adicionar um parâmetro de busca por nome, utilizando o operador "like" e o operador Nullish

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -8,7 +8,13 @@ class ProductController {
     // método para listar produtos
     try {
       // lógica para listar produtos
-      response.json({ message: "ok" }); // retorna resposta JSON com mensagem de sucesso
+      const { name } = request.query; // extrai o parâmetro de consulta 'name' da requisição
+      const products = await knex<ProductRepository>("products")
+        .select() //seleciona todos os produtos da tabela "products", importando o tipo ProductRepository para garantir a estrutura correta e
+        .whereLike("name", `%${name ?? ""}%`) // filtra os produtos pelo nome, se o parâmetro 'name' for fornecido, caso contrário, retorna todos os produtos
+        .orderBy("name"); // ordena por nome em ordem alfabética
+
+      response.json(products); // retorna resposta JSON com os produtos
     } catch (error) {
       // captura erros
       next(error); // passa o erro para o middleware de tratamento de erros
