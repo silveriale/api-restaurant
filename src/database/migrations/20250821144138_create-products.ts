829d1d4bab4afc4a27cc51e08172e2a212fb073b
silveriale <leticia.silveria.costa@gmail.com>
2025-08-21 11:55:16 -0300

    Criando a Migration de Produtos.
    - npm run knex -- migrate:latest

--- src/database/migrations/20250821144138_create-products.ts
+++ src/database/migrations/20250821144138_create-products.ts
@@ -1,10 +1,16 @@
 import type { Knex } from "knex";
 
-
 export async function up(knex: Knex): Promise<void> {
+  //Cria a tabela de produtos
+  await knex.schema.createTable("products", (table) => {
+    table.increments("id").primary(); // Chave primária
+    table.text("name").notNullable(); // Nome do produto
+    table.decimal("price").notNullable(); // Preço do produto
+    table.timestamp("created_at").defaultTo(knex.fn.now()); // Data de criação
+    table.timestamp("updated_at").defaultTo(knex.fn.now()); // Data de atualização
+  });
 }
-
 
 export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable("products"); // Remove a tabela de produtos caso necessário
 }
-
