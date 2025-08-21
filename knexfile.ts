f505c9cfaed63695132999649058484ca100e2d8
silveriale <leticia.silveria.costa@gmail.com>
2025-08-21 11:48:59 -0300

    Habilitando Restrição de Relacionamentos
    - habilita a restrição de chaves estrangeiras no Knex.js
    - Configura o knexfile.ts usando o hook afterCreate para executar pragma foreign_keys = ON
    - Garante a consistência dos dados e o respeito às relações entre tabelas, evitando a inserção de dados inconsistentes.

--- knexfile.ts
+++ knexfile.ts
@@ -1,7 +1,15 @@
+import { connect } from "http2";
+
 export default {
   client: "sqlite3", // define o cliente do banco de dados como SQLite
   connection: {
     filename: "./src/database/database.sqlite", // caminho do arquivo do banco de dados
+  },
+  pool: {
+    afterCreate: (connection: any, done: any) => {
+      connection.run("PRAGMA foreign_keys = ON"); // habilita chaves estrangeiras no SQLite
+      done(); // chama o callback para indicar que a conexão está pronta
+    },
   },
   useNullAsDefault: true, // usa null como padrão para valores não definidos
   migrations: {
