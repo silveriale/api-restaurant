a65848a18e55ecf69a23955cd2854fbfdbd98faa
silveriale <leticia.silveria.costa@gmail.com>
2025-08-22 10:39:03 -0300

    Regras de negócio do pedido
    - validações para adicionar itens a uma sessão de mesa
    - importância de verificar se a sessão ou mesa está aberta ou fechada antes de adicionar um produto
    - verificações de existência da sessão, status de fechamento da mesa e existência do produto
    - como selecionar um produto e a importância de verificar sua existência para obter seu preço

--- src/controllers/orders-controller.ts
+++ src/controllers/orders-controller.ts
@@ -1,20 +1,43 @@
 import { Request, Response, NextFunction } from "express";
+import { AppError } from "@/utils/AppError";
+import { knex } from "@/database/knex"; // Importa o knex para interagir com o banco de dados
 import { z } from "zod";
 
 class OrdersController {
   async create(request: Request, response: Response, next: NextFunction) {
     try {
       const bodySchema = z.object({
-        table_session_id: z.number(),
-        product_id: z.number(),
-        quantity: z.number(),
+        // define o schema de validação do corpo da requisição
+        table_session_id: z.number(), // id da sessão da mesa
+        product_id: z.number(), // id do produto
+        quantity: z.number(), // quantidade do produto
       });
 
       const { table_session_id, product_id, quantity } = bodySchema.parse(
         request.body
-      );
+      ); // valida o corpo da requisição e extrai os dados
 
-      return response.status(201).json();
+      const session = await knex<TablesSessionsRepository>("tables_sessions") // verifica se a sessão da mesa existe e está aberta
+        .where({ id: table_session_id }) // filtra pela id da sessão da mesa
+        .first(); // pega a primeira sessão que encontrar
+
+      if (!session) {
+        throw new AppError("A sessão de mesa não existe.");
+      } // se a sessão da mesa não existir, lança um erro
+
+      if (session.closed_at) {
+        throw new AppError("A sessão de mesa já foi fechada.");
+      } // se a sessão da mesa estiver fechada, lança um erro
+
+      const product = await knex<ProductRepository>("products") // verifica se o produto existe
+        .where({ id: product_id }) // filtra pela id do produto
+        .first(); // pega o primeiro produto que encontrar
+
+      if (!product) {
+        throw new AppError("O produto não existe.");
+      } // se o produto não existir, lança um erro
+
+      return response.status(201).json(product);
     } catch (error) {
       next(error);
     }
